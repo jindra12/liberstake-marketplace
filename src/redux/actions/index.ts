17bfd921e612cc2abba99b289a40438a093c83af
jindra12 <jindra12.underdark@gmail.com>
2025-07-28 21:40:05 +0200

    Remove more files, change project to typescript

---
+++ src/redux/actions/index.ts
@@ -0,0 +1,19 @@
+import * as authActions from './auth';
+import * as walletActions from './wallet';
+import * as blockchainActions from './blockchain';
+import * as validatorActions from './validator';
+import * as dexActions from './dex';
+import * as contractsActions from './contracts';
+import * as nftsActions from './nfts';
+import * as ethActions from './eth';
+
+export {
+  authActions,
+  walletActions,
+  blockchainActions,
+  validatorActions,
+  dexActions,
+  contractsActions,
+  nftsActions,
+  ethActions,
+};
